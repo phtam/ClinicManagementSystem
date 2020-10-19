0edad043fea960450be2eee5d25c14e733d7abf5
Hoàng Tâm <59590158+phtam@users.noreply.github.com>
2020-10-19 08:55:18 +0700

    Quản lý Payments

---
+++ ClinicManagementSystem/Areas/Admin/Controllers/PaymentsController.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using ClinicManagementSystem.EF;
+
+namespace ClinicManagementSystem.Areas.Admin.Controllers
+{
+    public class PaymentsController : Controller
+    {
+        private ClinicData db = new ClinicData();
+
+        // GET: Admin/Payments
+        public ActionResult Index()
+        {
+            return View(db.Payments.ToList());
+        }
+
+        // GET: Admin/Payments/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Payment payment = db.Payments.Find(id);
+            if (payment == null)
+            {
+                return HttpNotFound();
+            }
+            return View(payment);
+        }
+
+        // GET: Admin/Payments/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Admin/Payments/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "PaymentID,PaymentName")] Payment payment)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Payments.Add(payment);
+                if (db.SaveChanges() > 0)
+                    TempData["Notice_Create_Success"] = true;
+                
+                return RedirectToAction("Index");
+            }
+
+            return View(payment);
+        }
+
+        // GET: Admin/Payments/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Payment payment = db.Payments.Find(id);
+            if (payment == null)
+            {
+                return HttpNotFound();
+            }
+            return View(payment);
+        }
+
+        // POST: Admin/Payments/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "PaymentID,PaymentName")] Payment payment)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(payment).State = EntityState.Modified;
+                if (db.SaveChanges() > 0)
+                    TempData["Notice_Save_Success"] = true;
+                return RedirectToAction("Index");
+            }
+            return View(payment);
+        }
+
+        // GET: Admin/Payments/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Payment payment = db.Payments.Find(id);
+            if (payment == null)
+            {
+                return HttpNotFound();
+            }
+            return View(payment);
+        }
+
+        // POST: Admin/Payments/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            
+            try
+            {
+                Payment payment = db.Payments.Find(id);
+                db.Payments.Remove(payment);
+                if (db.SaveChanges() > 0)
+                    TempData["Notice_Delete_Success"] = true;
+            }
+            catch (Exception)
+            {
+                TempData["Notice_Delete_Fail"] = true;
+            }
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
